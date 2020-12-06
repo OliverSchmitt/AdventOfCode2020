1a35265a4444da93f56ae365d464e288057414ae
Oliver Schmitt <oliverschmitt@gmx.de>
2020-12-06 09:54:59 +0100

    Add day06

---
+++ day06/day06.py
@@ -0,0 +1,25 @@
+import sys
+sys.path.insert(0, "..")
+
+import misc
+
+
+def parse_lines(lines):
+    c = []
+    for line in lines:
+        c.append(line)
+    return c
+
+
+def part1(content):
+    pass
+
+
+def part2(content):
+    pass
+
+
+if __name__ == "__main__":
+    content = parse_lines(misc.read_content())
+    part1(content)
+    part2(content)
